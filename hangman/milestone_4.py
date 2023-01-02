c6453580792526097e577871f76e097f9ac89a4a
Pradnya Joshi <pradxj07@users.noreply.github.com>
2023-01-02 16:05:26 +0000

    Update milestone_4.py

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -15,13 +15,21 @@ class Hangman():
 
     def check_guess(self,guess):
         guess1 = guess.lower() 
+        letterList = list(self.word)
         if (guess1 in self.word):
             print("Good guess! ",guess1 ,"is in the word.")
+            for letterIdx,letter in enumerate(self.word):
+                if (letter == guess):
+                    self.word_guessed[letterIdx]=letter
+                print(letterIdx)
+            self.num_letters -= 1
+            print(self.word_guessed, " / ", self.num_letters)
+
         # else:
         #     print("Sorry, ",guess1, " is not in the word. Try again.")
 
     def ask_for_input(self): 
-        while True:
+        while self.num_letters > 0:
             guess = input("Enter a letter")
             if not(len(guess) == 1 and  guess.isalpha() ):
                 # break
